a8beb2c2a793e92e8bfaa848c856074b66e88ef9
Odin <alepiojo05@gmail.com>
2015-04-17 22:38:18 -0600

    asdf

---
+++ src/progra1/Progra1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra1;
+/**
+ *
+ * @author Alejandro
+ */
+public class Progra1 {
+    Counter counter;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Interfaz nuevoCounter = new Interfaz();
+        nuevoCounter.setVisible(true);
+    }
+    
+}
